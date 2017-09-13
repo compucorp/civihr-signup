94fbbd1a157b34a9b6135796916a9836424634b4
mickadoo <michaeldevery@gmail.com>
2017-09-13 10:51:02 +0100

    PCHR-2474: Use museo sans font from typekit

--- js/civihr-signup.js
+++ js/civihr-signup.js
@@ -2,4 +2,10 @@ CRM.$(document).ready(function() {
   CRM.$('#signup-hide-button').click(function() {
     CRM.$('#civihr-signup-tab').toggleClass('slide-in-right');
   });
+
+  try{
+    Typekit.load({ async: true });
+  }catch(e){
+    // all is lost
+  }
 });
