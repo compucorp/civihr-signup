d6012877338e649f478c2c68b32d634a6aeff220
Michael Devery <michael@compucorp.co.uk>
2017-09-28 15:27:20 +0100

    PCHR-2685: Update text for signup tab

--- templates/signup-block.tpl.php
+++ templates/signup-block.tpl.php
@@ -6,9 +6,10 @@
     <form target="_blank" action="https://civihr.org/signup">
         <h3>Become an early adopter</h3>
         <p>
-            Sign up for the public beta phase before the end of September.
-            We'll provide free hosting and support till the end of March 2018,
-            and you'll get to help shape the future of CiviHR.
+            You can use CiviHR online for free while it is in public beta up
+            until the end of March 2018. Thereafter it will be charged at £1
+            per user per month with unpaid volunteers remaining free of charge.
+            You can cancel at any time and take away a copy of your data.
         </p>
         <button class="signup-button big-and-bold rounded">SIGN ME UP!</button>
         <p>or <a href="https://civihr.org">return to civihr.org</a></p>
